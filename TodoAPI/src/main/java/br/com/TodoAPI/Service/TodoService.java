2654a5794960881195b392b97f0301ceb51ca7e3
Lucas Lopes <lucasalvarengalopes@gmail.com>
2024-08-23 00:47:04 -0300

    Feature: TodoService unit tests

--- TodoAPI/src/main/java/br/com/TodoAPI/Service/TodoService.java
+++ TodoAPI/src/main/java/br/com/TodoAPI/Service/TodoService.java
@@ -1,53 +1,52 @@
 package br.com.TodoAPI.Service;
-import java.util.List;
-import java.util.stream.Collectors;
+
 import br.com.TodoAPI.Dto.Request.TodoRequestDTO;
 import br.com.TodoAPI.Dto.Response.TodoResponseDTO;
+import br.com.TodoAPI.Model.Todo;
+import br.com.TodoAPI.Repository.TodoRepository;
 import br.com.TodoAPI.util.CommonMethods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.TodoAPI.Model.Todo;
-import br.com.TodoAPI.Repository.TodoRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TodoService {
-    
-    @Autowired
-    private TodoRepository todoRepository;
-
-    public List<TodoResponseDTO> findAll() {
-        return todoRepository.findAll().stream().map(TodoResponseDTO::new).collect(Collectors.toList());
-    }
-
-    public TodoResponseDTO findById(Long id) {
-        return new TodoResponseDTO(CommonMethods.getEntityById(id, todoRepository));
-    }
-
-    public TodoResponseDTO createTodo(TodoRequestDTO todoRequestDTO) {
-
-        Todo todo = new Todo(todoRequestDTO);
-
-        todoRepository.save(todo);
-
-        return new TodoResponseDTO(todo);
-    }
-
-    public TodoResponseDTO updateTodo(Long id, TodoRequestDTO todoRequestDTO) {
-
-        Todo updatedTodo = CommonMethods.getEntityById(id, todoRepository);
-
-        updatedTodo.setTitle(todoRequestDTO.title());
-        updatedTodo.setDone(todoRequestDTO.done());
-
-        todoRepository.save(updatedTodo);
-
-        return new TodoResponseDTO(updatedTodo);
-    }
-
-    public void delete(Long id) {
-        Todo deletedTodo = CommonMethods.getEntityById(id, todoRepository);
-
-        todoRepository.delete(deletedTodo);
-    }
+	
+	@Autowired
+	private TodoRepository todoRepository;
+	
+	public List<TodoResponseDTO> findAll() {
+		return todoRepository.findAll().stream().map(TodoResponseDTO::new).collect(Collectors.toList());
+	}
+	
+	public TodoResponseDTO findById(Long id) {
+		return new TodoResponseDTO(CommonMethods.getEntityById(id, todoRepository));
+	}
+	
+	public TodoResponseDTO createTodo(TodoRequestDTO todoRequestDTO) {
+		
+		Todo todo = new Todo(todoRequestDTO);
+		
+		return new TodoResponseDTO(todoRepository.save(todo));
+	}
+	
+	public TodoResponseDTO updateTodo(Long id, TodoRequestDTO todoRequestDTO) {
+		
+		Todo updatedTodo = CommonMethods.getEntityById(id, todoRepository);
+		
+		updatedTodo.setTitle(todoRequestDTO.title());
+		updatedTodo.setDone(todoRequestDTO.done());
+		
+		todoRepository.save(updatedTodo);
+		
+		return new TodoResponseDTO(updatedTodo);
+	}
+	
+	public void delete(Long id) {
+		Todo deletedTodo = CommonMethods.getEntityById(id, todoRepository);
+		
+		todoRepository.delete(deletedTodo);
+	}
 }
--- TodoAPI/src/test/java/br/com/TodoAPI/UnitTest/a.java
+++
@@ -1,4 +0,0 @@
-package br.com.TodoAPI.UnitTest;
-
-public class a {
-}
